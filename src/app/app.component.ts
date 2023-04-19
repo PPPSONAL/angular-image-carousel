3c76ae03b9eb3e20c5f3bad52da64849f965fa51
Sonal Panchal <sonal.panchal@exponentia.ai>
2023-04-19 22:32:24 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DataConstants } from './data';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-image-carousel';
+  @ViewChild('pptContent', { read: ElementRef }) pptContent!: ElementRef;
+  scrollLeftcount: number = 0;
+  scrollRightcount: number = 1;
+  data = DataConstants.fileList
+  
+  
+  checkEnd(e: any){
+    this.scrollLeftcount = this.pptContent.nativeElement.scrollLeft;
+    this.scrollRightcount = Math.round((this.pptContent.nativeElement.scrollWidth - this.pptContent.nativeElement.clientWidth) - this.pptContent.nativeElement.scrollLeft)
+  }
+
+  public scrollRight(): void {
+    this.pptContent.nativeElement.scrollTo({ left: (this.pptContent.nativeElement.scrollLeft + 300), behavior: 'smooth' });
+  }
+
+  public scrollLeft(): void {
+    this.pptContent.nativeElement.scrollTo({ left: (this.pptContent.nativeElement.scrollLeft - 300), behavior: 'smooth' });
+  }
+  
+  checkVar(ppt: any){
+    return ppt.length != 0 ? ppt[0].thumbnail_path : ''
+  }
 }
