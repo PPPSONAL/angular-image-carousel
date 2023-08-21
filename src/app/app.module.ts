c77f0a0a8a06ee00ff416b881ff8597de95d9c50
Sonal Panchal <sonal.panchal@exponentia.ai>
2023-08-21 15:47:31 +0530

    pannel resizer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SafePipe } from './safe.pipe';
+import { ResizerThreeComponent } from './resizer-three/resizer-three.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ResizerThreeComponent,
     SafePipe
   ],
   imports: [
